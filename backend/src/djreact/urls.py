379b874350d341d718f257f2fc852f787a43ec9e
Ozzie Cunningham <oswald3.cunningham@gmail.com>
2020-04-16 16:00:36 -0400

    create serializer

--- backend/src/djreact/urls.py
+++ backend/src/djreact/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import include, path
-
+from django.urls import path, include
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
+    path('api/', include('articles.api.urls'))
+
 ]
