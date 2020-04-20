86f8af2d211c6805739e13482f1b120ad63f7239
Ozzie Cunningham <oswald3.cunningham@gmail.com>
2020-04-20 10:46:50 -0400

    clean up layout

--- frontend/gui/src/components/Article.js
+++ frontend/gui/src/components/Article.js
@@ -22,11 +22,7 @@ const Articles = props => {
         pageSize: 3
       }}
       dataSource={props.data}
-      footer={
-        <div>
-          <b>ant design</b> footer part
-        </div>
-      }
+  
       renderItem={item => (
         <List.Item
           key={item.title}
@@ -57,7 +53,7 @@ const Articles = props => {
         >
           <List.Item.Meta
             avatar={<Avatar src={item.avatar} />}
-            title={<a href={item.href}>{item.title}</a>}
+            title={<a href={`/${item.id}`}>{item.title}</a>}
             description={item.description}
           />
           {item.content}
